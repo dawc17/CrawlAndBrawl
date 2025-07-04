63d47129300726b648f3cb80883f079c4f6a84bd
dawc17 <dawid.czaplicki2008@gmail.com>
2025-07-04 13:02:31 +0200

    +++

--- src/main.py
+++ src/main.py
@@ -2,15 +2,12 @@ import pygame
 import sys
 from settings import *
 
-# Initialize
 pygame.init()
 
-# Settings
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Crawl & Brawl")
 clock = pygame.time.Clock()
 
-# Game loop
 def main():
     running = True
     while running:
@@ -19,9 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Update
-        # Draw
-        screen.fill((0, 0, 0))  # black background
+        screen.fill((0, 0, 0))
         pygame.display.flip()
 
     pygame.quit()
